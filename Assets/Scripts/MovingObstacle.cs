3f18a36dfbfe6f2a591d754db77db9ea3dc5444d
bryonyl <bryonyrlarson@gmail.com>
2024-12-17 12:48:30 +0000

    Started adding moving obstacles

---
+++ Assets/Scripts/MovingObstacle.cs
@@ -0,0 +1,52 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour
+{
+    [SerializeField] private Transform m_startWayPoint;
+    [SerializeField] private Transform m_endWayPoint;
+    [SerializeField] private float m_speed = 5;
+
+    private Transform m_target; // Holds the start/end point, so the object knows where to go to
+
+    void ChangeTarget()
+    {
+        if (m_target = m_startWayPoint)
+        {
+            m_target = m_endWayPoint;
+        }
+        else
+        {
+            m_target = m_startWayPoint;
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        m_target = m_startWayPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Moves an object towards a target destination, never overshooting
+        transform.position = Vector2.MoveTowards(transform.position, m_target.position, m_speed * Time.deltaTime); // Time.deltaTime is the time in seconds between the last frame and the current frame.
+                                                                                                                       // When using * Time.deltaTime, you can carry out actions over time - this is useful
+                                                                                                                       // when it comes to timers and fire rates, for example.
+    }
+    private void OnTriggerEnter2D(Collider2D collision) // Trigger/overlap check to see if we touch anything
+                                                        // The collision parameter can be used to get info about what we hit, e.g. whether it's a tag, a layer, components, a script...
+    {
+        if (collision.CompareTag("MovingObstacleWaypoint")) // Checking the tag of the thing we have overlapped with. If there is a tag, call ChangeTarget function
+        {
+            ChangeTarget();
+        }
+
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player is attacked!");
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private int m_playerMaxHealth = 100;
+    [SerializeField] private int m_playerCurrentHealth = 100;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
