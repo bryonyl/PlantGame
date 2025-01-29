bc7ddb0041fdc1919f5f3597c3c3364f6fad98f4
bryonyl <bryonyrlarson@gmail.com>
2025-01-29 20:44:10 +0000

    Worked on inventory system

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -1,8 +1,11 @@
- using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
     public InventorySlot[] inventorySlots;
+    public GameObject inventoryItemPrefab;
 
     // Called from the outside, gets information about what item should be added and will search for any unoccupied slot in the inventory
     public void AddItem(Item item)
@@ -10,13 +13,21 @@ public class InventoryManager : MonoBehaviour
         // Find any empty slot
         for (int i = 0; i < inventorySlots.Length; i++)
         {
-            //Inventory slot = inventorySlots[i]; -- 13:15 in tut video
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot == null)
+            {
+                SpawnNewItem(item, slot);
+                return;
+            }
         }
     }
 
     // When item is found from AddItem, then this method, SpawnNewItem, will spawn a new item in the inventory slot using info from AddItem
     void SpawnNewItem(Item item, InventorySlot slot)
     {
-
+        GameObject newItemGo = Instantiate(inventoryItemPrefab, slot.transform);
+        InventoryItem inventoryItem = newItemGo.GetComponent<InventoryItem>();
+        InventoryItem.InitialiseItem(item);
     }
 }
